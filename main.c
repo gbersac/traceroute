a9ce4e4f7c7683e0a58210f17780b43e73dd1be8
Guillaume Bersac <bersac_1@hotmail.fr>
2015-11-28 20:12:15 +0100

    clean sources to minimum

--- main.c
+++ main.c
@@ -18,6 +18,7 @@ typedef struct sockaddr_in s_sockaddr_in;
 typedef struct sockaddr s_sockaddr;
 typedef struct icmphdr s_icmphdr;
 
+// add timeval ?
 typedef struct	packet
 {
 	s_icmphdr	hdr;
@@ -47,70 +48,11 @@ unsigned short checksum(void *b, int len)
 }
 
 /*--------------------------------------------------------------------*/
-/*--- display - present echo info                                  ---*/
-/*--------------------------------------------------------------------*/
-void display(void *buf, int bytes)
-{
-	printf("display\n");
-	int i;
-	struct iphdr *ip = buf;
-	s_icmphdr *icmp = buf+ip->ihl*4;
-
-	printf("----------------\n");
-	for ( i = 0; i < bytes; i++ )
-	{
-		if ( !(i & 15) )
-			printf("\nX:  ");
-		printf("X ");
-	}
-	printf("\n");
-	printf("IPv%d: hdr-size=%d pkt-size=%d protocol=%d TTL=%d",
-		ip->version, ip->ihl*4, ntohs(ip->tot_len), ip->protocol,
-		ip->ttl);
-	// printf("dst=%s\n", inet_ntoa(ip->daddr));
-	if ( icmp->un.echo.id == pid )
-	{
-		printf("ICMP: type[%d/%d] checksum[%d] id[%d] seq[%d]\n",
-			icmp->type, icmp->code, ntohs(icmp->checksum),
-			icmp->un.echo.id, icmp->un.echo.sequence);
-	}
-}
-
-/*--------------------------------------------------------------------*/
-/*--- listener - separate process to listen for and collect messages--*/
-/*--------------------------------------------------------------------*/
-void listener(void)
-{
-	int sd;
-	s_sockaddr_in addr;
-	unsigned char buf[1024];
-
-	sd = socket(PF_INET, SOCK_RAW, proto->p_proto);
-	if ( sd < 0 )
-	{
-		perror("socket");
-		exit(0);
-	}
-	while (42) {
-		int bytes, len = sizeof(addr);
-		bzero(buf, sizeof(buf));
-		bytes = recvfrom(sd, buf, sizeof(buf), 0, (s_sockaddr*)&addr, (socklen_t *)&len);
-		printf("ici\n");
-		if ( bytes <= 0 )
-			perror("recvfrom");
-		else
-			display(buf, bytes);
-	}
-	exit(0);
-}
-
-/*--------------------------------------------------------------------*/
 /*--- ping - Create message and send it.                           ---*/
 /*--------------------------------------------------------------------*/
 void ping(s_sockaddr_in *addr)
 {
 	const int val=255;
-	unsigned long i;
 	int sd, cnt=1;
 	s_packet pckt;
 	s_sockaddr_in r_addr;
@@ -125,21 +67,21 @@ void ping(s_sockaddr_in *addr)
 	if ( fcntl(sd, F_SETFL, O_NONBLOCK) != 0 )
 		perror("Request nonblocking I/O");
 	while (42) {
-		int len=sizeof(r_addr);
+		int len = sizeof(r_addr);
 
+		// send message
 		printf("Msg #%d\n", cnt);
-		if ( recvfrom(sd, &pckt, sizeof(pckt), 0, (s_sockaddr*)&r_addr, (socklen_t *)&len) > 0 )
-			printf("***Got message!***\n");
 		bzero(&pckt, sizeof(pckt));
 		pckt.hdr.type = ICMP_ECHO;
 		pckt.hdr.un.echo.id = pid;
-		for ( i = 0; i < sizeof(pckt.msg)-1; i++ )
-			pckt.msg[i] = i+'0';
-		pckt.msg[i] = 0;
 		pckt.hdr.un.echo.sequence = cnt++;
 		pckt.hdr.checksum = checksum(&pckt, sizeof(pckt));
 		if ( sendto(sd, &pckt, sizeof(pckt), 0, (s_sockaddr*)addr, sizeof(*addr)) <= 0 )
 			perror("sendto");
+
+		// receive message
+		if ( recvfrom(sd, &pckt, sizeof(pckt), 0, (s_sockaddr*)&r_addr, (socklen_t *)&len) > 0 )
+			printf("***Got message!***\n");
 		sleep(1);
 	}
 }
@@ -165,10 +107,6 @@ int main(int argc, char *argv[])
 	addr.sin_family = hname->h_addrtype;
 	addr.sin_port = 0;
 	addr.sin_addr.s_addr = *(long*)hname->h_addr;
-	if ( fork() != 0 )
-		ping(&addr);
-	else
-		listener();
-	wait(0);
+	ping(&addr);
 	return 0;
 }
