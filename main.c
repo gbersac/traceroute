19cdcffc7c93fc66f3bf9a895499dd90c5b544ef
Guillaume Bersac <bersac_1@hotmail.fr>
2015-11-28 17:39:10 +0100

    test with one example

--- main.c
+++ main.c
@@ -1,125 +1,165 @@
-#include <errno.h>
-#include <unistd.h>
-#include <netdb.h>
-#include <sys/socket.h>
-#include <sys/types.h>
 #include <netinet/in.h>
-#include <netinet/ip_icmp.h>
-
-#include <string.h>
-#include <stdlib.h>
+#include <arpa/inet.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <unistd.h>
 
-#define DEFAULT_NB_PACKET 100
-#define PACKET_SIZE 80
+#define NB_PACKET 20
 
-void usage()
+typedef unsigned char u8;
+typedef unsigned short int u16;
+
+unsigned short in_cksum(unsigned short *ptr, int nbytes);
+void help(const char *p);
+
+int main(int argc, char **argv)
 {
-	printf("Usage: ft_ping ip_adress\n");
+    if (argc < 3)
+    {
+        printf("usage: %s <source IP> <destination IP> [payload size]\n", argv[0]);
+        exit(0);
+    }
+
+    unsigned long daddr;
+    unsigned long saddr;
+    int payload_size = 0, sent, sent_size;
+
+    saddr = inet_addr(argv[1]);
+    daddr = inet_addr(argv[2]);
+
+    if (argc > 3)
+    {
+        payload_size = atoi(argv[3]);
+    }
+
+    //Raw socket - if you use IPPROTO_ICMP, then kernel will fill in the correct ICMP header checksum, if IPPROTO_RAW, then it wont
+    int sockfd = socket (AF_INET, SOCK_RAW, IPPROTO_RAW);
+
+    if (sockfd < 0)
+    {
+        perror("could not create socket");
+        return (0);
+    }
+
+    int on = 1;
+
+    // We shall provide IP headers
+    if (setsockopt (sockfd, IPPROTO_IP, IP_HDRINCL, (const char*)&on, sizeof (on)) == -1)
+    {
+        perror("setsockopt");
+        return (0);
+    }
+
+    //allow socket to send datagrams to broadcast addresses
+    if (setsockopt (sockfd, SOL_SOCKET, SO_BROADCAST, (const char*)&on, sizeof (on)) == -1)
+    {
+        perror("setsockopt");
+        return (0);
+    }
+
+    //Calculate total packet size
+    int packet_size = sizeof (struct iphdr) + sizeof (struct icmphdr) + payload_size;
+    char *packet = (char *) malloc (packet_size);
+
+    if (!packet)
+    {
+        perror("out of memory");
+        close(sockfd);
+        return (0);
+    }
+
+    //ip header
+    struct iphdr *ip = (struct iphdr *) packet;
+    struct icmphdr *icmp = (struct icmphdr *) (packet + sizeof (struct iphdr));
+
+    //zero out the packet buffer
+    memset (packet, 0, packet_size);
+
+    ip->version = 4;
+    ip->ihl = 5;
+    ip->tos = 0;
+    ip->tot_len = htons (packet_size);
+    ip->id = rand ();
+    ip->frag_off = 0;
+    ip->ttl = 255;
+    ip->protocol = IPPROTO_ICMP;
+    ip->saddr = saddr;
+    ip->daddr = daddr;
+    //ip->check = in_cksum ((u16 *) ip, sizeof (struct iphdr));
+
+    icmp->type = ICMP_ECHO;
+    icmp->code = 0;
+    icmp->un.echo.sequence = rand();
+    icmp->un.echo.id = rand();
+    //checksum
+    icmp->checksum = 0;
+
+    struct sockaddr_in servaddr;
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = daddr;
+    memset(&servaddr.sin_zero, 0, sizeof (servaddr.sin_zero));
+
+    puts("flooding...");
+
+    for (sent = 0 ; sent < NB_PACKET ; ++sent)
+    {
+        memset(packet + sizeof(struct iphdr) + sizeof(struct icmphdr), rand() % 255, payload_size);
+
+        //recalculate the icmp header checksum since we are filling the payload with random characters everytime
+        icmp->checksum = 0;
+        icmp->checksum = in_cksum((unsigned short *)icmp, sizeof(struct icmphdr) + payload_size);
+
+        if ( (sent_size = sendto(sockfd, packet, packet_size, 0, (struct sockaddr*) &servaddr, sizeof (servaddr))) < 1)
+        {
+            perror("send failed\n");
+            break;
+        }
+        ++sent;
+        printf("%d packets sent\r", sent + 1);
+        fflush(stdout);
+
+        int bytes = sendto(sockfd, packet, sizeof(packet), 0, (sockaddr *)&pingaddr, sizeof(sockaddr_in));
+
+        bytes = recvmsg(sockfd, inbuf	, sizeof(inbuf), 0, (sockaddr *)&pingaddr, (socklen_t *)&addrlen);
+
+        usleep(10000);  //microseconds
+    }
+    printf("\n");
+
+    free(packet);
+    close(sockfd);
+
+    return (0);
 }
 
-// void get_ip_from_str(char const str[], void *dst)
-// {
-
-// 	int s = inet_pton(AF_INET, str, dst);
-// 	if (s <= 0)	{
-// 		if (s == 0)
-// 			fprintf(stderr, "Not in presentation format");
-// 		else
-// 			perror("inet_pton");
-// 		exit(EXIT_FAILURE);
-// 	}
-// }
-
-// Computing the internet checksum (RFC 1071).
-// Note that the internet checksum does not preclude collisions.
-uint16_t checksum (uint16_t *addr, int len)
+/*
+    Function calculate checksum
+*/
+unsigned short in_cksum(unsigned short *ptr, int nbytes)
 {
-  int count = len;
-  register uint32_t sum = 0;
-  uint16_t answer = 0;
+    register long sum;
+    u_short oddbyte;
+    register u_short answer;
 
-  // Sum up 2-byte values until none or only one byte left.
-  while (count > 1) {
-    sum += *(addr++);
-    count -= 2;
-  }
+    sum = 0;
+    while (nbytes > 1) {
+        sum += *ptr++;
+        nbytes -= 2;
+    }
 
-  // Add left-over byte, if any.
-  if (count > 0) {
-    sum += *(uint8_t *) addr;
-  }
+    if (nbytes == 1) {
+        oddbyte = 0;
+        *((u_char *) & oddbyte) = *(u_char *) ptr;
+        sum += oddbyte;
+    }
 
-  // Fold 32-bit sum into 16 bits; we lose information by doing this,
-  // increasing the chances of a collision.
-  // sum = (lower 16 bits) + (upper 16 bits shifted right 16 bits)
-  while (sum >> 16) {
-    sum = (sum & 0xffff) + (sum >> 16);
-  }
+    sum = (sum >> 16) + (sum & 0xffff);
+    sum += (sum >> 16);
+    answer = ~sum;
 
-  // Checksum is one's compliment of sum.
-  answer = ~sum;
-
-  return (answer);
-}
-
-int main(int argc, char const *argv[])
-{
-	if (argc == 0){
-		usage();
-	}
-
-	// create socket
-	int fd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
-	if (fd == -1) {
-	    printf("%s", strerror(errno));
-	    exit(EXIT_FAILURE);
-	}
-
-	// Option so that the ip header will be constructed by the user.
-	int hdrincl = 1;
-	if (setsockopt(fd, IPPROTO_IP, IP_HDRINCL, &hdrincl, sizeof(hdrincl)) == -1) {
-	    printf("%s", strerror(errno));
-	    exit(EXIT_FAILURE);
-	}
-
-	// create icmp header for data
-	const size_t req_size=8;
-	struct icmphdr req;
-	req.type= ICMP_ECHO;
-	req.code = 0;
-	req.checksum = 0;
-	req.un.echo.id = htons(rand());
-	req.un.echo.sequence = htons(1);
-	req.checksum = checksum(&req, req_size);
-
-	// create address
-	struct addrinfo hints;
-	struct addrinfo *result, *rp;
-	memset(&hints, 0, sizeof(struct addrinfo));
-	hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
-	hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
-	hints.ai_flags = 0;
-	hints.ai_protocol = 0;          /* Any protocol */
-	s = getaddrinfo(argv[1], argv[2], &hints, &result);
-	if (s != 0) {
-		printf("getaddrinfo: %s\n", gai_strerror(s));
-		exit(EXIT_FAILURE);
-	}
-
-	// send the data
-	if (sendto(fd,
-			&req,
-			req_size,
-			0,
-	    	res->ai_addr,
-	    	res->ai_addrlen) == -1) {
-	    printf("%s",strerror(errno));
-	exit(EXIT_FAILURE);
-	}
-
-	// receive the callback
-	bytes = recvfrom(fd, inbuf, PACKET_SIZE, 0, (sockaddr *)&pingaddr, (socklen_t *)&addrlen);
-
-	return EXIT_SUCCESS;
+    return (answer);
 }
